2133b8d2036f86b8e128e646849822b0236be04c
danielweatrowski <dwweats@gmail.com>
2023-03-10 13:25:03 -0800

    added adapters and boxscore pitching info

---
+++ mlbapp/Domain/Services/BoxscoreAdapter.swift
@@ -0,0 +1,8 @@
+//
+//  BoxscoreAdapter.swift
+//  mlbapp
+//
+//  Created by Daniel Weatrowski on 3/10/23.
+//
+
+import Foundation
---
+++ mlbapp/Domain/Services/LinescoreAdapter.swift
@@ -0,0 +1,8 @@
+//
+//  LinescoreAdapter.swift
+//  mlbapp
+//
+//  Created by Daniel Weatrowski on 3/10/23.
+//
+
+import Foundation
